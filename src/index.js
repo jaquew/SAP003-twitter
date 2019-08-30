2b5f62e2209e2f996d87b4a228306814e388300e
jaquew <eng.jaquesilva.com>
2019-08-30 17:33:55 -0300

    desativar botao e ajustes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-for(i=1; i<localStorage.length;i++){
-  document.getElementById("timeline").innerHTML+= "<li>" + localStorage.getItem(i) + "</li>"
+// Desativa o botão se o campo se tiver vazio
+document.getElementById("text").addEventListener("keyup", btn)
+function btn(){
+  if (document.getElementById("text").value===""){
+    document.getElementById("btn-send").disabled = true;
+  } else{
+    document.getElementById("btn-send").disabled = false;
+  }
 }
 
-document.getElementById("btn-send").addEventListener("click", clickCounter)
+// imprime os tweets que já estão salvos
+for(i=1; i<localStorage.length;i++){
+  document.getElementById("timeline").innerHTML += "<li>" + localStorage.getItem(i) + "</li>"
+}
 
-// função para contar os cliques e armazenar no local storage 
+// função para contar os cliques e armazenar no local storage
+document.getElementById("btn-send").addEventListener("click", clickCounter)
+ 
 function clickCounter() {
   if (localStorage.clickcount) {
     localStorage.clickcount = Number(localStorage.clickcount)+1;
@@ -17,8 +28,6 @@ function clickCounter() {
 function postatxt() {
   let text = document.getElementById("text").value;
   localStorage.setItem(localStorage.clickcount, text);
-
-  document.getElementById("timeline").value=localStorage.getItem
 
   document.getElementById("timeline").innerHTML+= "<li>"+localStorage.getItem(localStorage.clickcount) + "</li> "
 }
